d599472fc72a08c5d75a31d7130fe19038a7875d
Nate <nreynold@gmail.com>
2014-12-06 06:28:48 -0500

    Working version with graphical updates

--- hud.js
+++ hud.js
@@ -1,33 +1,61 @@
-var userNodeID;
-var div;
+var sceneNode = vwf.find("","/")[0];
+
 
 vwf_view.satProperty = function(nodeId, propertyName, propertyValue){
-//	if(nodeId == userNodeID){
-		switch(propertyName){
-			case "healthMultiplier":
-				div = document.getElementById('enemyHealth');
-				div.innerHTML = '<p>Enemy Health: ' + propertyValue + '</p>';
-				break;
-			case "moveSpeed":
-				div = document.getElementById('enemyMoveSpeed');
-				div.innerHTML = '<p>Enemy Speed: ' + propertyValue + '</p>';
-				break; 
-			case "enemyCount":
-				div = document.getElementById('enemies');
-				div.innerHTML = '<p>Enemies: ' + propertyValue + '</p>';
-				break; 
-			case "enemiesKilled":
-				div = document.getElementById('enemiesKilled');
-				div.innerHTML = '<p>Enemies Killed: ' + propertyValue + '</p>';
-				break; 
-		}
-//	}
-};
-
-vwf_view.initializedNode = function(nodeID, childID, childExtendsID, childImplementsIDs,
-            childSource, childType, childIndex, childName, callback) {
-    var navObjectName = this.kernel.moniker();
-    if ( childName == navObjectName ) {
-    	userNodeID = nodeID;
-    }
+	switch(propertyName){
+		case "playerCount":
+			alert('player count changed to: '+propertyValue);
+			break;
+		case "countDown":
+			div = document.getElementById('overlay');
+			if(propertyValue>0){
+				div.innerHTML = '<h1>Game starts in: ' + propertyValue + ' </h1> <h2> Wave: 0 </h2>';
+			}
+			else if(propertyValue==0) {
+				div.innerHTML = '<h1> RELEASE THE SLOTHS! </h1> <h2> Wave: 1 </h2>';
+			}
+			break;
+		case "waveCounter":
+			div = document.getElementById('overlay');
+			div.innerHTML = '<h1> RELEASE THE SLOTHS! </h1> <h2> Wave: '+propertyValue+' </h2>';
+			break;
+		case "enemyCount":
+			div = document.getElementById('enemies');
+			div.innerHTML = '<p>Enemies left: ' + propertyValue + '</p>';
+			break;			 
+		case "enemiesKilled":
+			div = document.getElementById('enemiesKilled');
+			div.innerHTML = '<p> <img src=\"UI_Elements/SlothIcon.png\" height=\"40px\">  ' + propertyValue + '</p>';
+			break; 
+		case "shotsUntilDowngrade":
+			div = document.getElementById('numberBulletsLeft');
+			div.innerHTML = '<p> <img src=\"UI_Elements/Bullets.png\" height=\"40px\"> ' + propertyValue + ' </p>';
+			break;
+		case "livesRemaining":
+			div = document.getElementById('livesRemaining');
+			div.innerHTML = '<p>Lives remaining: ' + propertyValue + '</p>';
+			if(propertyValue === 0){
+				div = document.getElementById('overlay');
+				div.innerHTML = '<h1> Game Over! </h1> ';
+			}
+			break;
+		case "playerHealth":
+			var clients = vwf_view.kernel.findClients("", "/*");
+			var players = vwf_view.kernel.find(vwf_view.kernel.find("","/")[0], "./element(*,'http://vwf.example.com/navigable.vwf')");
+			for(var i = 0; i < players.length; i++) {
+				var found = false;
+	            for (var j = 0; j < clients.length; j++) {
+	                if(vwf_view.kernel.name(players[i]).indexOf(vwf_view.kernel.name(clients[j])) >= 0) {
+	                    div = document.getElementById('health');
+						div.innerHTML = div.innerHTML = '<p> <img src=\"UI_Elements/Health.png\" height=\"40px\"> ' + propertyValue + '</p>';
+						found = true;
+						break;
+	                }
+	            if(found === true){
+	            	break;
+	            }
+            }
+            break;
+        }
+	}
 };
--- index-model.js
+++ index-model.js
@@ -1,27 +1,45 @@
+//Countdown until game starts
+var countDown = 10;
+
+//Template for enemies
 var enemy = {
     extends: "http://vwf.example.com/node3.vwf",
     source: "models/Sloth.dae",
     properties: {
         enabled: false,
         visible: false,
-		scale: .5,
+		scale: .2,
         health: 1.0
     },
     children: {
         material: {
           extends: "http://vwf.example.com/material.vwf",
           properties:{
-			transparent: true
+		  transparent: true
           }
         }
     }
 };
 
+//Template for powerups
+var aPowerUp = {
+    extends: "http://vwf.example.com/node3.vwf",
+    source: "models/SpecialAmmo.dae",
+    properties: {
+      translation: [2,4,0],
+      visible: false,
+	  scale: .3
+    },
+    children: {
+      material: {
+      },
+    },
+};
 
-//Creates the bullet object
+//Template for bullets
 var aBullet = {
     extends: "http://vwf.example.com/node3.vwf",
-    source: "models/ball.dae",
+    source: "models/Orange.dae",
     properties: {
         enabled: false,
         visible: false,
@@ -30,26 +48,30 @@ var aBullet = {
         xSpeed: 0,
         ySpeed: 0,
         Speed: 10,
-        scale: 0.5,
+        scale: .1
     },
     children: {
         material: {
             extends: "http://vwf.example.com/material.vwf",
             properties: {
-                color: "#ff0000",
+			  transparent: true
             },
         },
     },
 };
 
-this.initialize = function() {
-    this.future( 0 ).initializeBullets();
-    this.future( 0 ).initializeEnemy();
-    this.future( 0 ).createEnemy();
-    this.future( this.healthMultiplierTimer ).increaseEnemyMoveSpeed();
-    this.future( this.moveSpeedTimer ).increaseEnemyHealth();
+this.initialize = function() 
+{
+	//Delay is added to ensure player initialization doesn't interrupt proper enemy initialization
+    this.future(0).count_to_zero();
+	this.future(30).initializeBullets();
+	this.future(30).initializeEnemy();
+	this.future(30).createEnemy();
+	this.future(30).initializePowerUp();
+	this.future(30).createPowerUp();
 }
 
+//creates 50 bullets of the aBullet template
 this.initializeBullets = function(){
     for(var i = 0; i < 50; i++){
         var newBullet = $.extend(true, {}, aBullet);
@@ -58,58 +80,94 @@ this.initializeBullets = function(){
     }
 }
 
+//Creates 10 enemies of the enemy template
 this.initializeEnemy = function() {
-    //Creates a list of unused enemies for use by the program
     for(var i = 0; i < 10; i++ ){
         var newEnemy = $.extend(true, {}, enemy);
-        this.enemies.children.create("Enemy"+this.enemyCount, newEnemy);
-        this.enemyCount += 1;
+        this.enemies.children.create("Enemy"+i, newEnemy);
     }
 }
 
+this.count_to_zero = function() 
+{
+	if(this.countDown>0) {
+		this.countDown--;
+		this.future(1).count_to_zero();
+	}
+}
 
+//creates the powerup
+this.initializePowerUp = function(){
+    var newPowerUp = $.extend(true, {}, aPowerUp);
+    this.powerUps.children.create("powerUp", newPowerUp);
+}
 
 //Adds an enemy on the screen every 2 seconds if there are not the max number of enemies
 this.createEnemy = function(){
-    var newEnemy = this.findUnusedEnemy(); //Check to see if we can add a new enemy
-    if(newEnemy){
-        newEnemy.visible = true;
-        newEnemy.enabled = true;
-        newEnemy.health = this.healthMultiplier; 
+    if(this.totalEnemies >= this.enemyCount){
+        //Do nothing
+    }
+    else{
+        var newEnemy = this.findUnusedEnemy(); //Check to see if we can add a new enemy
+        if(newEnemy){
+            newEnemy.visible = true;
+            newEnemy.enabled = true;
+            newEnemy.health = this.healthMultiplier;
+            //Randomize the enemy's starting position between -500 and 500 for x and y
+            var xPos = Math.random() * 500;
+            var yPos = Math.random() * 500;
+            if(Math.random() < 0.5){
+                xPos = xPos * -1;
+            }
+            if(Math.random() < 0.5){
+                yPos = yPos * -1;
+            }
 
-        //Randomize the enemy's starting position between -500 and 500 for x and y
-        var xPos = Math.random() * 500;
-        var yPos = Math.random() * 500;
-        if(Math.random() < 0.5){
-            xPos = xPos * -1;
+            newEnemy.translation = [xPos, yPos, 0];
+
+            var closestPlayer = this.calculateClosestPlayer(newEnemy);
+            this.calculateEnemyMovement(closestPlayer, newEnemy);
+
+            this.totalEnemies++;
         }
-        if(Math.random() < 0.5){
-            yPos = yPos * -1;
-        }
-
-        //Place the enemy on the board
-        newEnemy.translation = [xPos, yPos, 0];
-        
-        //Find the closest player to the enemy and start moving towards it
-        var closestPlayer = this.calculateClosestPlayer(newEnemy);
-        this.calculateEnemyMovement(closestPlayer, newEnemy);
-
-        this.enemyCount++;
     }
 
     this.future(2).createEnemy();
 }
 
-//Increases the enemy's health by 1 after a set period of time
-this.increaseEnemyHealth = function(){
-    this.healthMultiplier = this.healthMultiplier + 1;
-    this.future( this.healthMultiplierTimer ).increaseEnemyHealth();
+//adds a powerup to the field, if none exists, every 5 seconds
+this.createPowerUp = function(){
+    if(this.powerUps.powerUp.visible === false){
+      //Randomize the powerup's starting position between -500 and 500 for x and y
+      var xPos = Math.random() * 500;
+      var yPos = Math.random() * 500;
+      if(Math.random() < 0.5){
+          xPos = xPos * -1;
+      }
+      if(Math.random() < 0.5){
+          yPos = yPos * -1;
+      }
+      this.powerUps.powerUp.translation = [xPos, yPos, 0];
+      this.powerUps.powerUp.visible = true;
+    }
+    this.future(5).createPowerUp();
 }
 
-//Increases the enemy's movement speed by 0.1 after a set period of time
+//Increases enemy health over time
+this.increaseEnemyHealth = function(){
+    this.healthMultiplier = this.healthMultiplier + 0.5;
+}
+
+//Increases enemy speed over time
 this.increaseEnemyMoveSpeed = function(){
-    this.moveSpeed = this.moveSpeed +0.1;
-    this.future( this.moveSpeedTimer ).increaseEnemyMoveSpeed();
+    this.moveSpeed = this.moveSpeed + 0.2;
+}
+
+this.incrementWave = function(){
+    this.increaseEnemyMoveSpeed();
+    this.increaseEnemyHealth();
+    this.enemyCount = 20;
+    this.waveCounter++;
 }
 
 //Returns a list of all of the players currently connected
@@ -129,19 +187,17 @@ this.findUnusedEnemy = function(){
 }
 
 //Finds the closest player to an enemy
-//Returns the closest player to the object, undefined otherwise
 this.calculateClosestPlayer = function(enemy){
     var closestPlayer;
     var currentDistance;
     var shortestDistance = 99999;
     var xDistance, yDistance;
     var listOfPlayers = this.findPlayers();
-
+    //Loop through the list of players; if a player is closer than previously calculated players, they become the closest player
     if(listOfPlayers){
         for(var i = 0; i < listOfPlayers.length; i++){
             xDistance = enemy.translation[0] - listOfPlayers[i].translation[0];
             yDistance = enemy.translation[1] - listOfPlayers[i].translation[1];
-
             currentDistance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
             if(currentDistance < shortestDistance){
                 shortestDistance = currentDistance;
@@ -153,6 +209,7 @@ this.calculateClosestPlayer = function(enemy){
     return undefined;
 }
 
+//Handles enemy movement AI
 this.calculateEnemyMovement = function(closestPlayer, enemy) {
     if(!closestPlayer){
         closestPlayer = this.calculateClosestPlayer(enemy);
@@ -160,74 +217,80 @@ this.calculateEnemyMovement = function(closestPlayer, enemy) {
     else if(enemy.visible === true){
         var xDistance = enemy.translation[0] - closestPlayer.translation[0];
         var yDistance = enemy.translation[1] - closestPlayer.translation[1];
-
         var distance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
 
-        //Checks to see if the enemy needs to move
-        if(distance < 10){      
-            this.enemyHitsPlayer(closestPlayer, enemy);  
+        //Checks to see if the enemy is close enough to hit the player
+        if(distance < 10){
+            this.enemyHitsPlayer(closestPlayer, enemy);
         }
         else if(xDistance !== 0 || yDistance !== 0){
             //Checks what direction it needs to move in
             if(Math.abs(xDistance) > Math.abs(yDistance)){
                 if(xDistance < 0){
-                    enemy.translateBy([this.moveSpeed, 0, 0]);
+                    enemy.translateBy([this.moveSpeed, 0, 0]); //Moves in the positive x direction
                 }
                 else{
-                    enemy.translateBy([-this.moveSpeed, 0 ,0]);
+                    enemy.translateBy([-this.moveSpeed, 0 ,0]); //Moves in the negative x direction
                 }
             }
             else{
                 if(yDistance < 0){
-                    enemy.translateBy([0, this.moveSpeed, 0]);
+                    enemy.translateBy([0, this.moveSpeed, 0]); //Move in the positive y direction
                 }
                 else{
-                    enemy.translateBy([0, -this.moveSpeed, 0]);
+                    enemy.translateBy([0, -this.moveSpeed, 0]); //Move in the negative y direction
                 }
-            }   
+            }
         }
         this.future( 1/30 ).calculateEnemyMovement(closestPlayer, enemy);
     }
 }
 
-//Takes a bullet and checks each enemy to see if it is close enough to register a hit
-//Returns true if it hit an enemy, false otherwise
+//Checks hit detection between bullet and each enemy
 this.checkIfHitEnemy = function(bullet){
     var enemies = this.enemies.children;
-    //Interates through the list of all enemies
     for(var i = 0; i < enemies.length; i++){
         if(enemies[i].visible === true){
-            //Check to see if the bullet hit an enemy
-            if(Math.abs(bullet.translation[0] - enemies[i].translation[0] - 120) < this.distanceForCollision &&
-               Math.abs(bullet.translation[1] - enemies[i].translation[1]) < this.distanceForCollision){
-                console.log("I hit an enemy!");
-                enemies[i].health = enemies[i].health - 1;
-                //If the enemy's health drops below 1, disable it
+            if(Math.abs(bullet.translation[0] - enemies[i].translation[0]) < 12 &&
+               Math.abs(bullet.translation[1] - enemies[i].translation[1]) < 12){
+                //Subtract the enemy's health
+                enemies[i].health = enemies[i].health - this.bulletMode;
+                //Detect if the enemy has died
                 if(enemies[i].health < 1){
                     enemies[i].visible = false;
                     enemies[i].enabled = false;
                     this.enemyCount--;
+                    this.totalEnemies--;
+                    //If there are no more enemies left, increment the wave
+                    if(this.enemyCount === 0){
+                        this.incrementWave();
+                    }
                     this.enemiesKilled++;
                 } 
                 return true;
-            }    
+            }
         }
-    }   
+    }
 }
 
+//checks hit detection between enemy and a player
 this.enemyHitsPlayer = function (closestPlayer, enemy){
-    closestPlayer.health = closestPlayer.health - 1;
-    if(closestPlayer.health <= 0){
+    closestPlayer.playerHealth = closestPlayer.playerHealth - 1;
+    if(closestPlayer.playerHealth <= 0){
         this.playerDied(closestPlayer);
     }
 }
 
+//resets a player when they reach 0 health
 this.playerDied = function (closestPlayer){
     closestPlayer.translateTo( [0, 0, 0]);
     closestPlayer.numTimesDead = closestPlayer.numTimesDead + 1;
-    closestPlayer.health = 100;
+    closestPlayer.playerHealth = 100;
+    this.livesRemaining--;
 }
 
+//move a bullet; if it connects with an enemy or powerup, handle that collision
+//if the bullet reaches the edges of the playspace, it deactivates
 this.moveBullet = function( bullet ){
     bullet.translateBy([bullet.xSpeed, bullet.ySpeed, 0]);
     if(bullet.translation[0] > 1000 || bullet.translation[0] < -1000 || bullet.translation[1] > 1000 || bullet.translation[1] < -1000){
@@ -240,39 +303,57 @@ this.moveBullet = function( bullet ){
         bullet.enabled = false;
         bullet.visible = false;
         bullet.xSpeed = 0;
-        bullet.ySpeed = 0;   
+        bullet.ySpeed = 0;
+    }
+    if(this.checkIfHitPowerUp(bullet) === true){
+        this.bulletMode = this.bulletMode + 1;
+        this.shotsUntilDowngrade = this.shotsUntilDowngrade + 10;
+        this.powerUps.powerUp.visible = false;
     }
     if(bullet.enabled){
         this.future( 1/30 ).moveBullet(bullet);
     }
 }
 
+//takes a bullet, moves it to a player's position, and sends it in the direction of the mouseclick's global position
+this.fire = function(newBull, playerPlace, globalPosition, thePlayer) {
+    newBull.translateTo([playerPlace[0],playerPlace[1],0]);
 
-this.fire = function( newBull, playerPlace, globalPosition, thePlayer) {
-    newBull.translateTo([playerPlace[0] +120,playerPlace[1],0]);
+	
+	if(this.bulletMode>1)
+	{
+		newBull.material.texture = "models/textures/coconut.png";
+		newBull.properties.scale = .2;
+	}
+	else
+	{
+		newBull.material.texture = "models/textures/orange.png";
+		newBull.properties.scale = .1;
+	}
+	
     newBull.visible = true;
-    var xDistance = playerPlace[0] + 240 - globalPosition[0];
+    var xDistance = playerPlace[0] - globalPosition[0];
     var yDistance = playerPlace[1] - globalPosition[1];
     var totalDistance = Math.sqrt((xDistance * xDistance) + (yDistance * yDistance));
     newBull.xSpeed = -newBull.Speed * (xDistance/totalDistance);
     newBull.ySpeed = -newBull.Speed * (yDistance/totalDistance);
-    //thePlayer.fireTime = 25;
+    //thePlayer.fireTime = 25; //line to reset timer on firing
     this.future( 1/30 ).moveBullet(newBull);
 }
 
+//event handler for left mouse click -- fires a bullet from the player
 this.pointerClick = function( input ) {
     var players = this.findPlayers();
     var playerFired;
     var index;
     for(index = 0; index < players.length; index++){
-      if(players[index].id.indexOf(this.client) > 0){
+      if(players[index].id.indexOf(this.client) > 0){ //is this the player that fired?
         playerFired = players[index];
       }
     }
-    if(playerFired.fireTime > 0){
+    if(playerFired.fireTime > 0){ //if the player has fired too recently, they can't fire right now (UNIMPLEMENTED)
       return;
     }
-
     var pi = input;
     var playerPlace = playerFired.translation;
     var listOfBullets = this.bullet.children;
@@ -280,19 +361,44 @@ this.pointerClick = function( input ) {
     var test = true;
     var i = 0;
     while(test){
-        if(listOfBullets[i].enabled === false){
+        if(listOfBullets[i].enabled === false){ //find an unused bullet
             coolBullet = listOfBullets[i];
             coolBullet.enabled = true;
             test = false;
         }
         i++;
-        
+
         if(i>listOfBullets.length){
-            test = false;
+              test = false;
         }
     }
-    this.fire(coolBullet, playerPlace, input.globalPosition, playerFired);
+    this.future(j * 2/30).fire(coolBullet, playerPlace, input.globalPosition, playerFired);
+    //this.future(j * 2/30) delays each bullet after the 0th to space out each shot
+    //.fire( ... ) fires the bullet just generated
 
+    if(this.shotsUntilDowngrade > 1){
+      this.shotsUntilDowngrade--;
+    }
+    else if(this.shotsUntilDowngrade == 1){
+      this.shotsUntilDowngrade--;
+      this.bulletMode--;
+      if(this.bulletMode > 1){
+        this.shotsUntilDowngrade = 1;
+      }
+    }
 }
 
-//@ sourceURL=index-model.js
+//keeps track of the closest player to powerup
+//if close enough, player picks up powerup
+this.checkIfHitPowerUp = function(bullet) {
+    var powerUp = this.powerUps.children;
+    for(var i = 0; i < powerUp.length; i++){
+        if(powerUp[i].visible === true){
+            if(Math.abs(bullet.translation[0] - powerUp[i].translation[0]) < 12 &&
+               Math.abs(bullet.translation[1] - powerUp[i].translation[1]) < 12){
+                console.log("I hit a powerup!");
+                return true;
+            }
+        }
+    }
+}
