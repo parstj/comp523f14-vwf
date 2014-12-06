af69e64f96610adbffedd3ae9a878fe04ffe13e8
TJ Parsons <parstj@gmail.com>
2014-12-06 00:25:06 -0500

    TJ's final update
    Health now updates, Game over condition

--- hud.js
+++ hud.js
@@ -1,5 +1,6 @@
 var sceneNode = vwf.find("","/")[0];
 
+
 vwf_view.satProperty = function(nodeId, propertyName, propertyValue){
 	switch(propertyName){
 		case "waveCounter":
@@ -10,14 +11,6 @@ vwf_view.satProperty = function(nodeId, propertyName, propertyValue){
 			div = document.getElementById('enemies');
 			div.innerHTML = '<p>Enemies left: ' + propertyValue + '</p>';
 			break;			 
-		// case "healthMultiplier":
-		// 	div = document.getElementById('enemyHealth');
-		// 	div.innerHTML = '<p>Enemy Health: ' + propertyValue + '</p>';
-		// 	break;
-		// case "moveSpeed":
-		// 	div = document.getElementById('enemyMoveSpeed');
-		// 	div.innerHTML = '<p>Enemy Speed: ' + propertyValue + '</p>';
-		// 	break; 
 		case "enemiesKilled":
 			div = document.getElementById('enemiesKilled');
 			div.innerHTML = '<p>Enemies Killed: ' + propertyValue + '</p>';
@@ -30,27 +23,31 @@ vwf_view.satProperty = function(nodeId, propertyName, propertyValue){
 		 	div = document.getElementById('bulletMultiplier');
 			div.innerHTML = '<p>Damage multiplier: x' + propertyValue + '</p>';
 			break;
-	}
-};
+		case "livesRemaining":
+			div = document.getElementById('livesRemaining');
+			div.innerHTML = '<p>Lives remaining: ' + propertyValue + '</p>';
+			if(propertyValue === 0){
+				alert("Game over!");
+			}
+			break;
+		case "playerHealth":
+			var clients = vwf_view.kernel.findClients("", "/*");
+			var players = vwf_view.kernel.find(vwf_view.kernel.find("","/")[0], "./element(*,'http://vwf.example.com/navigable.vwf')");
 
-vwf_view.deletedNode = function ( nodeID ) { 
-    if ( nodeID.slice(0, 33) == "http-vwf-example-com-clients-vwf:" ) {
-        // There is currently no way to match the deleted client to its associated navigation object
-        // so loop over all the navobjects and set the one without a client to disconnected
-        var players = vwf_view.kernel.find(vwf_view.kernel.find("","/")[0], "./element(*,'http://vwf.example.com/navigable.vwf')");
-        var clients = vwf_view.kernel.findClients("", "/*");
-
-        for(var i = 0; i < players.length; i++) {
-            var clientFound = false;
-            for (var j = 0; j < clients.length; j++) {
-                if(vwf_view.kernel.name(players[i]).indexOf(vwf_view.kernel.name(clients[j])) >= 0) {
-                    clientFound = true;
-                    break;
-                }
+			for(var i = 0; i < players.length; i++) {
+				var found = false;
+	            for (var j = 0; j < clients.length; j++) {
+	                if(vwf_view.kernel.name(players[i]).indexOf(vwf_view.kernel.name(clients[j])) >= 0) {
+	                    div = document.getElementById('health');
+						div.innerHTML = '<p>Health: ' + propertyValue + '</p>';
+						found = true;
+						break;
+	                }
+	            if(found === true){
+	            	break;
+	            }
             }
-            if(!clientFound) {
-                vwf_view.kernel.callMethod( sceneNode, "playerDisconnected", players[i]);
-            }
+            break;
         }
-    }
+	}
 };
